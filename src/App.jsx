cd065f2346314a2ecea3a49674021afd558dca6c
Sebastien <sebastienlato@gmail.com>
2025-11-22 20:05:59 -0500

    implement Photos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import gsap from "gsap";
 import { Draggable} from "gsap/Draggable";
 
-import { Navbar, Welcome, Dock} from '#components'
-import {Finder, Resume, Safari, Terminal, Text, Image, Contact} from "#windows";
+import {Navbar, Welcome, Dock, Home} from '#components'
+import {Finder, Resume, Safari, Terminal, Text, Image, Contact, Photos} from "#windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -20,6 +20,9 @@ const App = () => {
             <Text />
             <Image />
             <Contact />
+            <Photos />
+
+            <Home />
         </main>
     );
 };
